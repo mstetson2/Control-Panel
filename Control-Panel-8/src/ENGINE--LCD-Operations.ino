d73f81063915b357463abcf45d3ec06c24bf5e17
Matthew Stetson <mstetson717@gmail.com>
2017-04-10 20:55:12 -0500

    Ops work

--- Control-Panel-8/src/ENGINE--LCD-Operations.ino
+++ Control-Panel-8/src/ENGINE--LCD-Operations.ino
@@ -2,16 +2,35 @@
 
 //
 
-lcdData lcd_999 = {"WELCOME", "Version:","VERSION","By Matt Stetson"};
-lcdData lcd_000 = {"OPERATIONS:","E-STOP: OK","ERROR: OK","DISPATCH: UNKNOWN"};
-lcdData lcd_001 = {"STATUS 001: ","PANEL POWER OFF","333","444"};
+lcdData ops_999 = {"WELCOME", "Version:","VERSION","By Matt Stetson"};
+lcdData ops_000 = {"OPERATIONS:","E-STOP: OK","ERROR: OK","DISPATCH: UNKNOWN"};
+lcdData ops_001 = {"STATUS 001: ","PANEL POWER OFF","333","444"};
 
-lcdData lcd_100 = {"STATUS 100:"," "," ","DISPATCHING"};
+lcdData ops_100 = {"STATUS 100:"," "," ","DISPATCHING"};
+lcdData ops_109 = {""," "," ",""};
+lcdData ops_110 = {
+    "STATUS 110:",
+    "AIRGATES OPEN!",
+    "Close gates",
+    "to dispatch"
+};
+lcdData ops_120 = {
+    "STATUS 120:",
+    "RESTRAINTS OPEN!",
+    "Close restraints",
+    "to dispatch"
+};
 
-lcdData lcd_500 = {"STATUS 500:","NO CONTROL POWER"," ","PANEL TURNED OFF"};
-lcdData lcd_510 = {"STATUS 510:","E-STOP PRESSED"," ","Switch to BYPASS"};
-lcdData lcd_515 = {"STATUS 515:","E-STOP RESET:","To reset ESR:","Press ESR RESET"};
-lcdData lcd_520 = {"STATUS 520:","RIDE STOP PRESSED"," ","PRESS RIDE START"};
+lcdData ops_500 = {"STATUS 500:","NO CONTROL POWER"," ","PANEL TURNED OFF"};
+lcdData ops_510 = {"STATUS 510:","E-STOP PRESSED"," ","Switch to BYPASS"};
+lcdData ops_515 = {"STATUS 515:","E-STOP RESET:","To reset ESR:","Press ESR RESET"};
+lcdData ops_520 = {"STATUS 520:","RIDE STOP PRESSED"," ","PRESS RIDE START"};
+lcdData ops_800 = {
+    "WARNING!",
+    "",
+    "UNKNOWN OPS TYPE!",
+    ""
+};
 
 int lcdSet(int statusNumber) {
 	if(statusNumber == lcdStatus) {
@@ -21,28 +40,40 @@ int lcdSet(int statusNumber) {
 
 	switch(statusNumber) {
 		case 001:
-			lcdWrite(lcd_001);
+			lcdWrite(ops_001);
 			break;
 		case 100:
-			lcdWrite(lcd_100);
+			lcdWrite(ops_100);
+			break;
+		case 109:
+			lcdWrite(ops_109);
+			break;
+		case 110:
+			lcdWrite(ops_110);
+			break;
+		case 120:
+			lcdWrite(ops_120);
 			break;
 		case 500:
-			lcdWrite(lcd_500);
+			lcdWrite(ops_500);
 			break;
 		case 510:
-			lcdWrite(lcd_510);
+			lcdWrite(ops_510);
 			break;
 		case 515:
-			lcdWrite(lcd_515);
+			lcdWrite(ops_515);
 			break;
 		case 520:
-			lcdWrite(lcd_520);
+			lcdWrite(ops_520);
+			break;
+		case 800:
+			lcdWrite(ops_800);
 			break;
 		case 999:
-			lcdWrite(lcd_999);
+			lcdWrite(ops_999);
 			break;
 		default:
-			lcdWrite(lcd_000);
+			lcdWrite(ops_000);
 			break;
 	}
 	lcdStatus = statusNumber;
